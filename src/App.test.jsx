af97df7b2c15afe3cb14763ab5cf32233a7dda77
Shawn Sullivan <sullivansm3@gmail.com>
2020-03-13 08:19:15 -0600

    Comment out old test, create placeholder test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,12 +4,18 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-  );
+// test('renders text to DOM', () => {
+//   const { getByText } = render(
+//     <Provider store={store}>
+//       <App />
+//     </Provider>,
+//   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+//   expect(getByText('App')).toBeInTheDocument();
+// });
+
+test('2 + 2 = 4', () => {
+  const sum = 2 + 2;
+
+  expect(sum).toEqual(4);
 });
