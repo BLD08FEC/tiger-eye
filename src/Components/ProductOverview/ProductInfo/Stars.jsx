cc8ee34a571f9d77579cf3248508791441514123
Shawn Sullivan <sullivansm3@gmail.com>
2020-03-16 17:56:18 -0600

    Attempt to calculate Avg rating

--- src/Components/ProductOverview/ProductInfo/Stars.jsx
+++ src/Components/ProductOverview/ProductInfo/Stars.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import StarRatings from 'react-star-ratings';
 
 const Stars = ({ reviewMetaData }) => {
+  const calcAvgRating = (ratings) => {
+    let sum = 0;
+    let total = 0;
+    const average = null;
+    if (ratings !== undefined) {
+      const keys = Object.keys(ratings);
+      for (let i = 0; i < keys.length; i++) {
+        sum += (Number(keys[i]) * ratings[keys[i]]);
+        total += ratings[keys[i]];
+      }
+      console.log(total);
+    }
 
-  // const calAvgRating = (reviewMetaData) => {
-  // };
+    return average;
+  };
+  calcAvgRating(reviewMetaData.ratings);
+
   return (
     <div>
-      <p>*****</p>
-      {console.log(reviewMetaData.ratings)}
+      <StarRatings
+        rating={4.25}
+        starDimension="15px"
+        starSpacing="1px"
+      />
     </div>
-  )
+  );
 };
 
 const mapStateToProps = (state) => ({
