09b416ab3525fbe296a17a8014dff846b22080ef
Shawn Sullivan <sullivansm3@gmail.com>
2020-03-16 11:04:37 -0600

    Add types to get productData and productsList

--- src/data/types/types.jsx
+++ src/data/types/types.jsx
@@ -1,9 +1,13 @@
 /* eslint-disable import/prefer-default-export */
-export const CHANGE_COUNT = 'CHANGE_COUNT';
 export const CHANGE_PRODUCT_ID = 'CHANGE_PRODUCT_ID';
 export const GET_REVIEW_META_DATA = 'GET_REVIEW_META_DATA';
 export const GET_REVIEW_META_DATA_SUCCESS = 'GET_REVIEW_META_DATA_SUCCESS';
 export const GET_REVIEW_META_DATA_FAILURE = 'GET_REVIEW_META_DATA_FAILURE';
+export const GET_PRODUCT_DATA = 'GET_PRODUCT_DATA';
+export const GET_PRODUCT_DATA_SUCCESS = 'GET_PRODUCT_DATA_SUCCESS';
+export const GET_PRODUCTS_LIST = 'GET_PRODUCTS_LIST';
+export const GET_PRODUCTS_LIST_SUCCESS = 'GET_PRODUCTS_LIST_SUCCESS';
+
 
 // disabled the linter's perfer default export (export default CHANGE_COUNT)
 // The export didn't seem to work when using the preferred way.
