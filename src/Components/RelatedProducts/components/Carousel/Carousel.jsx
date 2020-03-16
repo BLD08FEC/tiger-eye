0e3d1fb2809fd07606f602a5d6936f1312190e3c
thomHayner <thom.hayner@gmail.com>
2020-03-16 06:23:01 -0600

    Add ESLint-config Lines for This File

--- src/Components/RelatedProducts/components/Carousel/Carousel.jsx
+++ src/Components/RelatedProducts/components/Carousel/Carousel.jsx
@@ -1,3 +1,7 @@
+/* eslint-disable react/no-access-state-in-setstate */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
 
 import ProductCard from '../ProductCard/ProductCard';
