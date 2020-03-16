9760e4dc8b2af19df66e392035ba53fc195174d0
thomHayner <thom.hayner@gmail.com>
2020-03-15 22:32:51 -0600

    Refactor for New Components - Carousel ProductCard

--- src/Components/RelatedProducts/RelatedProducts.jsx
+++ src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
-const RelatedProducts = () => (<div className="related-products">RelatedProducts PLACEHOLDER</div>);
+// import data from './dummyData';
+// import ProductCard from './components/ProductCard/ProductCard';
+import Carousel from './components/Carousel/Carousel';
+
+const RelatedProducts = () => (
+  <div className="container related-products">
+    <div className="row related-products-title"><em>You might also like...</em></div>
+    {/* <ProductCard data={data} /> */}
+    <Carousel className="row" />
+    <Carousel className="row" />
+  </div>
+);
 
 export default RelatedProducts;
