73036a4787b65b294cd42880e5d0024ce4096037
Michael Morahan <morahan.mike@gmail.com>
2020-03-16 04:16:41 -0600

    complete API connection draft file.  All connections created and
    exported.  Only console log to ensure proper connection.  Requires
    testing to ensure proper GET, POST, and PUT of info.

--- src/Shared/api.js
+++ src/Shared/api.js
@@ -1,4 +1,6 @@
 // API location: http://3.134.102.30/
+// connection info pulled from: https://gist.github.com/trentgoing/409c2d76ce8e187e2132e45d9bed4605#file-products_api-md
+
 // The different data services for this api are
     // Cart
     // Interactions
@@ -10,32 +12,51 @@
 // ======== Cart API =====================
 
 // get   = /cart/:user_token
+const getCart = (userToken, cb) => {
+    axios.get(`http://3.134.102.30/cart/${userToken}`)
+    .then(res => console.log(res.data))
+    // .then(res.data => cb(res.data))
+    // .catch(err => cb(error))
+}  
 
 // post = /cart/
+const addToCart = (userToken, cb) => {
+    axios.post(`http://3.134.102.30/cart/`)
+    .then(res => console.log(res.data))
+    // .then(res.data => cb(res.data))
+    // .catch(err => cb(error))
+}  
 
 // ======== Interactions API ==============
 
-// post = /interactions/
-
+const logInteraction = (userToken, cb) => {
+    axios.post(`http://3.134.102.30/interactions`)
+    .then(res => console.log(res.data))
+    // .then(res.data => cb(res.data))
+    // .catch(err => cb(error))
+}  
 
 // ======== Products =====================    
 // GET / products / list
 const getProducts = (productId, cb) => {
     axios.get('http://3.134.102.30/products/list')
-    .then(console.log(res.data))
+    .then(res => console.log(res.data))
     // .then(res.data => cb(res.data))
+    // .catch(err => cb(error))
 }    
 
 const getProduct = (productId, cb) => {
     axios.get(`http://3.134.102.30/products/${productId}`)
-    .then(console.log(res.data))
+    .then(res => console.log(res.data))
     // .then(res.data => cb(res.data))
+    // .catch(err => cb(error))
 }    
 
 const getProductStyle = (productId, cb) => {
     axios.get(`http://3.134.102.30/products/${productId}/styles`)
-    .then(console.log(res.data))
+    .then(res => console.log(res.data))
     // .then(res.data => cb(res.data))
+    // .catch(err => cb(error))
 }    
 
 
@@ -43,7 +64,7 @@ const getProductStyle = (productId, cb) => {
 
 const getRelatedProduct = (productId, cb) => {
     axios.get(`http://3.134.102.30/products/${productId}/related`)
-    .then(console.log(res.data))
+    .then(res => console.log(res.data))
     // .then(res.data => cb(res.data))
     // .catch(err => cb(error))
 }
@@ -54,27 +75,31 @@ const getRelatedProduct = (productId, cb) => {
 // Get 
 const getQuestions = (productId, cb) => {
     axios.get(`http://3.134.102.30/qa/${productId}`)
-    .then(console.log(res.data))
+    .then(res => console.log(res.data))
     // .then(res.data => cb(res.data))
+    // .catch(err => cb(error))
 }
 
 const getAnswers = (questionId, cb) => {
     axios.get(`http://3.134.102.30/qa/${questionId}/answers`)
-    .then(console.log(res.data))
+    .then(res => console.log(res.data))
     // .then(res.data => cb(res.data))
+    // .catch(err => cb(error))
 }
 
 // Post
 const postQuestion = (productId, cb) => {
     axios.post(`http://3.134.102.30/qa/${productId}`)
-    .then(console.log(res.data))
+    .then(res => console.log(res.data))
     // .then(res.data => cb(res.data))
+    // .catch(err => cb(error))
 }
 
 const postAnswer = (questionId, cb) => {
     axios.post(`http://3.134.102.30/qa/${questionId}/answers`)
-    .then(console.log(res.data))
+    .then(res => console.log(res.data))
     // .then(res.data => cb(res.data))
+    // .catch(err => cb(error))
 }
 
 // review Question 
@@ -83,29 +108,82 @@ const postAnswer = (questionId, cb) => {
 const markQuestionHelpful = (questionId, cb) => {
     axios.put(`http://3.134.102.30/qa/question/${questionId}/helpful`)
     .then(res => console.log(res.data))
+    // .then(res.data => cb(res.data))
+    // .catch(err => cb(error))
 }
 
 const reportQuestion = (questionId, cb) => {
     axios.put(`http://3.134.102.30/qa/question/${questionId}/report`)
     .then(res => console.log(res.data))
+    // .then(res.data => cb(res.data))
+    // .catch(err => cb(error))
 }
 
 // review Answer 
 // PUT / qa / answer /: question_id / helpful
 
-const markAnswerHelpful = (questionId, cb) => {
-    axios.put(`http://3.134.102.30/qa/answer/${questionId}/helpful`)
+const markAnswerHelpful = (answerId, cb) => {
+    axios.put(`http://3.134.102.30/qa/answer/${answerId}/helpful`)
     .then(res => console.log(res.data))
+    // .then(res.data => cb(res.data))
+    // .catch(err => cb(error))
 }
 
-const reportAnswer = (questionId, cb) => {
-    axios.put(`http://3.134.102.30/qa/answer/${questionId}/report`)
+const reportAnswer = (answerId, cb) => {
+    axios.put(`http://3.134.102.30/qa/answer/${answerId}/report`)
     .then(res => console.log(res.data))
+    // .then(res.data => cb(res.data))
+    // .catch(err => cb(error)) 
 }
+
 
 // =========== Reviews ============
+// GET /reviews/:product_id/list
+const getReviews = (productId, cb) => {
+    axios.get(`http://3.134.102.30/reviews/${productId}/list`)
+    .then(res => console.log(res.data))
+    // .then(res.data => cb(res.data))
+    // .catch(err => cb(error))
+}
+
+const getReviewMeta = (productId, cb) => {
+    axios.get(`http://3.134.102.30/reviews/${productId}/meta`)
+    .then(res => console.log(res.data))
+    // .then(res.data => cb(res.data))
+    // .catch(err => cb(error))
+}
+
+const postReview = (productId, cb) => {
+    axios.post(`http://3.134.102.30/reviews/${productId}`)
+    .then(res => console.log(res.data))
+    // .then(res.data => cb(res.data))
+    // .catch(err => cb(error))
+}
+
+const markReviewHelpful = (reviewId, cb) => {
+    axios.put(`http://3.134.102.30/reviews/helpful/${reviewId}`)
+    .then(res => console.log(res.data))
+    // .then(res.data => cb(res.data))
+    // .catch(err => cb(error))
+}
+
+const reportReview = (reviewId, cb) => {
+    axios.put(`http://3.134.102.30/reviews/report/${reviewId}`)
+    .then(res => console.log(res.data))
+    // .then(res.data => cb(res.data))
+    // .catch(err => cb(error))
+}
+
+
+// debating between Add vs Post for naming methods. 
+// ex: addQuestion / addAnswer vs postQuestion / postAnswer
+// applies to reviews & questions post methods
+// generally add is more intuitive but post is the type of request...
 
 module.exports(
+    logInteraction,
+    getCart,
+    addToCart,
     getProducts,
     getProduct,
     getProductStyle,
@@ -114,7 +192,13 @@ module.exports(
     getAnswers,
     postQuestion,
     postAnswer,
-    
-
-
+    markQuestionHelpful,
+    reportQuestion,
+    markAnswerHelpful,
+    reportAnswer,
+    getReviews,
+    getReviewMeta,
+    postReview,
+    markReviewHelpful,
+    reportReview
 )
