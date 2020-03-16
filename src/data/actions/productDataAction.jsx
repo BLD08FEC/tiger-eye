d288a05cb245347ba743cd89bc5c76e42e3fad1e
Shawn Sullivan <sullivansm3@gmail.com>
2020-03-16 11:06:14 -0600

    Clean up code

--- src/data/actions/productDataAction.jsx
+++ src/data/actions/productDataAction.jsx
@@ -2,19 +2,15 @@
 /* eslint-disable no-undef */
 import { GET_PRODUCT_DATA_SUCCESS } from '../types/types';
 
-export const getProductData = () => {
-  return (dispatch) => {
-    return fetch(`http://3.134.102.30/products/1`)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        return dispatch(getProductDataSuccess(data));
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-};
+export const getProductData = () => (dispatch) => fetch('http://3.134.102.30/products/1')
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    return dispatch(getProductDataSuccess(data));
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 export const getProductDataSuccess = (productData) => ({
   type: GET_PRODUCT_DATA_SUCCESS,
