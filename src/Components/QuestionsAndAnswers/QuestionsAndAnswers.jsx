375e3644170f52b58cb6e9d9d1f4b9d7812bdb80
Michael Morahan <morahan.mike@gmail.com>
2020-03-16 04:55:29 -0600

    solve linter errors and work on reformating
    code

--- src/Components/QuestionsAndAnswers/QuestionsAndAnswers.jsx
+++ src/Components/QuestionsAndAnswers/QuestionsAndAnswers.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+// import helpers from '../../Shared/api';
 // require helpers from '../../shared/api.js'
 // not sure how to import info from api calls
 
 const QuestionsAndAnswers = () => (
-    <div className="questions-and-answers">QuestionsAndAnswers PLACEHOLDER</div>
-
+  <div className="questions-and-answers">
+    {/* {helpers.getQuestions(2)} */}
+    QuestionsAndAnswers PLACEHOLDER
+  </div>
 );
 
 export default QuestionsAndAnswers;
