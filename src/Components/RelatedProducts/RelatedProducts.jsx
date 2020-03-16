b1fc19936a297496e466a7e655a75f5fbc656d5b
thomHayner <thom.hayner@gmail.com>
2020-03-16 05:33:53 -0600

    Code Clean Up

--- src/Components/RelatedProducts/RelatedProducts.jsx
+++ src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 
-// import data from './dummyData';
-// import ProductCard from './components/ProductCard/ProductCard';
 import Carousel from './components/Carousel/Carousel';
 
 const RelatedProducts = () => (
   <div className="container related-products">
     <div className="row related-products-title"><em>You might also like...</em></div>
-    {/* <ProductCard data={data} /> */}
-    <Carousel className="row" />
-    <Carousel className="row" />
+    <Carousel className="row related-products-carousel" />
+    <div className="row related-products-title"><em>Build your own outfit.</em></div>
+    <Carousel className="row my-outfit-carousel" />
   </div>
 );
 
