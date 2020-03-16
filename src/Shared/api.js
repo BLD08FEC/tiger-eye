57c235c4acb9abad54bffbd30ec58fc1301e361f
Michael Morahan <morahan.mike@gmail.com>
2020-03-16 03:50:50 -0600

    built out Q&A framework.  untested

--- src/Shared/api.js
+++ src/Shared/api.js
@@ -1,36 +1,43 @@
 // API location: http://3.134.102.30/
 // The different data services for this api are
-    // Products
-    // Reviews
-    // Questions & Answers
     // Cart
     // Interactions
+    // Products
+    // Questions & Answers
+    // Reviews
 
-// ======== Products =====================
-// get List of Products
+    
+// ======== Cart API =====================
+
+// get   = /cart/:user_token
+
+// post = /cart/
+
+// ======== Interactions API ==============
+
+// post = /interactions/
+
+
+// ======== Products =====================    
+// GET / products / list
 const getProducts = (productId, cb) => {
     axios.get('http://3.134.102.30/products/list')
     .then(console.log(res.data))
     // .then(res.data => cb(res.data))
-}
+}    
 
 const getProduct = (productId, cb) => {
     axios.get(`http://3.134.102.30/products/${productId}`)
     .then(console.log(res.data))
     // .then(res.data => cb(res.data))
-}
+}    
 
 const getProductStyle = (productId, cb) => {
     axios.get(`http://3.134.102.30/products/${productId}/styles`)
     .then(console.log(res.data))
     // .then(res.data => cb(res.data))
-}
+}    
 
-// ======== Cart API =====================
-
-// get
-
-// post
 
 // ======== Related Products ==============
 
@@ -60,15 +67,43 @@ const getAnswers = (questionId, cb) => {
 // Post
 const postQuestion = (productId, cb) => {
     axios.post(`http://3.134.102.30/qa/${productId}`)
-        .then(console.log(res.data))
+    .then(console.log(res.data))
     // .then(res.data => cb(res.data))
 }
 
 const postAnswer = (questionId, cb) => {
     axios.post(`http://3.134.102.30/qa/${questionId}/answers`)
-        .then(console.log(res.data))
+    .then(console.log(res.data))
     // .then(res.data => cb(res.data))
 }
+
+// review Question 
+// PUT / qa / question /: question_id / helpful
+
+const markQuestionHelpful = (questionId, cb) => {
+    axios.put(`http://3.134.102.30/qa/question/${questionId}/helpful`)
+    .then(res => console.log(res.data))
+}
+
+const reportQuestion = (questionId, cb) => {
+    axios.put(`http://3.134.102.30/qa/question/${questionId}/report`)
+    .then(res => console.log(res.data))
+}
+
+// review Answer 
+// PUT / qa / answer /: question_id / helpful
+
+const markAnswerHelpful = (questionId, cb) => {
+    axios.put(`http://3.134.102.30/qa/answer/${questionId}/helpful`)
+    .then(res => console.log(res.data))
+}
+
+const reportAnswer = (questionId, cb) => {
+    axios.put(`http://3.134.102.30/qa/answer/${questionId}/report`)
+    .then(res => console.log(res.data))
+}
+
+// =========== Reviews ============
 
 module.exports(
     getProducts,
