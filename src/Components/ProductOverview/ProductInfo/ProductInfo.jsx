f27730099a16fe3d73479ea3f861b1e6dbc6dbc3
Shawn Sullivan <sullivansm3@gmail.com>
2020-03-17 14:59:35 -0600

    Build out structure for productInfo

--- src/Components/ProductOverview/ProductInfo/ProductInfo.jsx
+++ src/Components/ProductOverview/ProductInfo/ProductInfo.jsx
@@ -1,20 +1,48 @@
+/* eslint-disable */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Stars from '../../../Shared/Stars';
 
-const ProductInfo = ({ reviewMetaData }) => (
-  <div>
-    Product Info
-    <br />
-    {reviewMetaData.product_id}
-    <br />
-    <Stars />
-    <a href="#reviews">Read all reviews</a>
+const ProductInfo = ({ productData }) => (
+  <div className="container-fluid">
+    <div className="row rating">
+      <Stars />
+      <a href="#reviews">Read all reviews</a>
+    </div>
+    <div className="row category">
+      {productData.category}
+    </div>
+    <div className="row product-name">
+      {productData.name}
+    </div>
+    <div className="row price">
+      {productData.default_price}
+    </div>
+    <div className="row styles">
+      styles will go here
+    </div>
+    <div className="row">
+      <div className="col-12 col-sm-6">
+        select size
+      </div>
+      <div className="col-12 col-sm-6">
+        quantity
+      </div>
+    </div>
+    <div className="row styles">
+      <div className="col-12 col-sm-8">
+        ADD TO BAG
+      </div>
+      <div className="col-12 col-sm-4">
+        FAVORITE
+      </div>
+    </div>
   </div>
 );
 
 const mapStateToProps = (state) => ({
   reviewMetaData: state.reviewMetaReducer.reviewMetaData,
+  productData: state.productDataReducer.productData,
 });
 
 // const mapDispatchToProps = (dispatch) => ({
