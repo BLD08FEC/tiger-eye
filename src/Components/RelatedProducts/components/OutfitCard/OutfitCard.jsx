7c1771fc29e3feab239e9ffdd0c3a99bf4e08022
thomHayner <thom.hayner@gmail.com>
2020-03-17 03:14:32 -0600

    Refactor Basic Component per Styleguide

--- src/Components/RelatedProducts/components/OutfitCard/OutfitCard.jsx
+++ src/Components/RelatedProducts/components/OutfitCard/OutfitCard.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import './OutfitCard.scss';
 
-const OutfitCard = () => {
-  return (
-    <div />
-  );
-};
+const OutfitCard = () => (<div />);
+
 
 export default OutfitCard;
