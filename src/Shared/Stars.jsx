d035f5fe4fb70f278509e2a327fb5ae10b9bb07a
Shawn Sullivan <sullivansm3@gmail.com>
2020-03-17 09:35:51 -0600

    Move Stars component to shared dir

--- src/Shared/Stars.jsx
+++ src/Shared/Stars.jsx
@@ -1,19 +1,16 @@
-/* eslint-disable */
 import React from 'react';
 import { connect } from 'react-redux';
 import StarRatings from 'react-star-ratings';
 
-const Stars = ({ avgRating }) => {
-  return (
-    <div>
-      <StarRatings
-        rating={avgRating}
-        starDimension="15px"
-        starSpacing="1px"
-      />
-    </div>
-  );
-};
+const Stars = ({ avgRating }) => (
+  <div>
+    <StarRatings
+      rating={avgRating}
+      starDimension="15px"
+      starSpacing="1px"
+    />
+  </div>
+);
 
 const mapStateToProps = (state) => ({
   reviewMetaData: state.reviewMetaReducer.reviewMetaData,
