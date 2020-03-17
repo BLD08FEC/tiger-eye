6c52dfe2191c8bbb8b469bd4a69d3ca43221eaea
Michael Morahan <morahan.mike@gmail.com>
2020-03-17 04:09:40 -0600

    solve linter error.

--- src/Components/QuestionsAndAnswers/Questions/QuestionList.jsx
+++ src/Components/QuestionsAndAnswers/Questions/QuestionList.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/destructuring-assignment */
 // eslint(react/no-unused-state)
 import React from 'react';
 import Question from './Question';
@@ -7,48 +8,47 @@ class QuestionList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      initialQuestion: 0,
       currentQuestion: 0,
       displayedQuestions: [],
     };
-    this.initialQuestion = this.initialQuestion.bind(this);
     this.currentQuestion = this.currentQuestion.bind(this);
     this.displayedQuestions = this.displayedQuestions.bind(this);
     // const productQuestions = data.results,
   }
 
 
-  default() {
-    let increment = this.state.currentQuestion;
-    this.setState(this.state.displayedQuestions.push(data.results[this.state.initialQuestion]));
-    this.setState(this.state.displayedQuestions.push(data.results[this.state.initialQuestion +1]));
-    increment += 2;
-    this.setState({ currentQuestion: increment });
+  default(state) {
+    this.setState(state.displayedQuestions.push(data.results[this.currentQuestion]));
+    this.incrementCurrentQuestion();
+    this.setState(state.displayedQuestions.push(data.results[this.currentQuestion]));
+    this.incrementCurrentQuestion();
   }
 
+  incrementCurrentQuestion() {
+    this.setState((state) => ({ currentQuestion: state.currentQuestion + 1 }));
+  }
 
   next() {
-    let increment = this.state.currentQuestion;
     const max = this.state.data.results.length;
     if (max > this.state.currentQuestion + 2) {
-      this.state.displayedQuestions.push(data.currentQuestion + 1);
-      this.state.displayedQuestions.push(data.currentQuestion + 2);
-      increment += 2;
+      this.state.displayedQuestions.push(data.results.currentQuestion);
+      this.incrementCurrentQuestion();
+      this.state.displayedQuestions.push(data.results.currentQuestion);
+      this.incrementCurrentQuestion();
     }
     if (max === this.state.currentQuestion + 1) {
       this.state.displayedQuestions.push(Question);
-      increment += 1;
+      this.incrementCurrentQuestion();
     } else {
       this.state.displayedQuestions.push('No more questions');
     }
-    this.setState({ currentQuestion: increment });
   }
 
   render() {
     return (
       <div className="row question-list">
-        <Question displayedQuestion={this.state.displayedQuestions[0].question_body} />
-        <Question displayedQuestion={this.state.displayedQuestions[1].question_body} />
+        <Question displayedQuestion={this.state.displayedQuestions[this.currentQuestion].question_body} />
+        <Question displayedQuestion={this.state.displayedQuestions[this.currentQuestion].question_body} />
       </div>
     );
   }
