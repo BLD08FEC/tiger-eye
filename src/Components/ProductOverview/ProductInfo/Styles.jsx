c86720954c46d8fad761ceaf346224e66890e262
Shawn Sullivan <sullivansm3@gmail.com>
2020-03-18 14:28:39 -0600

    Add selected and sale price to props

--- src/Components/ProductOverview/ProductInfo/Styles.jsx
+++ src/Components/ProductOverview/ProductInfo/Styles.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import '../ProductOverview.css';
 
 const Styles = ({ productStyles }) => (
   productStyles.product_id ? (
-    <div>
-      {productStyles.results.map((style, index) => (
-        <img src={style.photos[0].thumbnail_url} alt={style.name} className="rounded-circle w-25 style-pics" />
+    <div className="style-pics">
+      {productStyles.results.map((style) => (
+        <img src={style.photos[0].thumbnail_url} alt={style.name} className="rounded-circle w-25" key={style.style_id} />
       ))}
     </div>
   ) : <div />
-//   <div>styles</div>
 );
 
 
