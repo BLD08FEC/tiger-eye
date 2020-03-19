0dce750dfda13a44f332c17524533b4f1b73a167
Shawn Sullivan <sullivansm3@gmail.com>
2020-03-19 15:18:23 -0600

    Overlay checkmark on selected style

--- src/Components/ProductOverview/ProductInfo/Styles.jsx
+++ src/Components/ProductOverview/ProductInfo/Styles.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
@@ -9,18 +10,26 @@ import { updateSelectedStyle } from '../../../data/actions/productDataAction';
 
 const Styles = (props) => (
   props.productStyles.product_id ? (
-    <div>
+    <div className="select-checkmark">
       <div className="style-name">Style: {props.selectedStyleName}</div>
-      {props.productStyles.results.map((style, index) => (
-        <img
-          src={style.photos[0].thumbnail_url}
-          alt={style.name}
-          id={index}
-          className="rounded-circle style-pics"
-          key={style.style_id}
-          onClick={() => props.updateSelectedStyle(props.productStyles.results[index])}
-        />
-      ))}
+      <div>
+        {props.productStyles.results.map((style, index) => (
+          <label className="style-container" key={style.style_id}>
+            {props.selectedStyle_id === style.style_id
+              ? <input type="radio" name="style-selector" checked />
+              : <input type="radio" name="style-selector" />}
+            <span className="checkmark">✔</span>
+            <img
+              src={style.photos[0].thumbnail_url}
+              alt={style.name}
+              id={index}
+              className="rounded-circle style-pics"
+              name="style-selector"
+              onClick={() => props.updateSelectedStyle(props.productStyles.results[index])}
+            />
+          </label>
+        ))}
+      </div>
     </div>
   ) : <div />
 );
@@ -29,6 +38,7 @@ const Styles = (props) => (
 const mapStateToProps = (state) => ({
   productStyles: state.productDataReducer.productStyles,
   selectedStyleName: state.productDataReducer.selectedStyleName,
+  selectedStyle_id: state.productDataReducer.selectedStyle_id,
 });
 
 const mapDispatchToProps = (dispatch) => ({
