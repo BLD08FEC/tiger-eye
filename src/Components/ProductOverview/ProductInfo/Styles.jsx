db984e8cd0b29bd4980e1edc3125c3ad58d01d43
Shawn Sullivan <sullivansm3@gmail.com>
2020-03-18 19:10:08 -0600

    update selectedStyle when  a style is clicked

--- src/Components/ProductOverview/ProductInfo/Styles.jsx
+++ src/Components/ProductOverview/ProductInfo/Styles.jsx
@@ -1,12 +1,22 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React from 'react';
 import { connect } from 'react-redux';
 import '../ProductOverview.css';
+import { updateSelectedStyle } from '../../../data/actions/productDataAction';
 
-const Styles = ({ productStyles }) => (
-  productStyles.product_id ? (
+const Styles = (props) => (
+  props.productStyles.product_id ? (
     <div className="style-pics">
-      {productStyles.results.map((style) => (
-        <img src={style.photos[0].thumbnail_url} alt={style.name} className="rounded-circle w-25" key={style.style_id} />
+      {props.productStyles.results.map((style, index) => (
+        <img
+          src={style.photos[0].thumbnail_url}
+          alt={style.name}
+          id={index}
+          className="rounded-circle w-25"
+          key={style.style_id}
+          onClick={() => props.updateSelectedStyle(props.productStyles.results[index])}
+        />
       ))}
     </div>
   ) : <div />
@@ -17,5 +27,9 @@ const mapStateToProps = (state) => ({
   productStyles: state.productDataReducer.productStyles,
 });
 
+const mapDispatchToProps = (dispatch) => ({
+  updateSelectedStyle: (defaultStyle) => dispatch(updateSelectedStyle(defaultStyle)),
+});
 
-export default connect(mapStateToProps)(Styles);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Styles);
