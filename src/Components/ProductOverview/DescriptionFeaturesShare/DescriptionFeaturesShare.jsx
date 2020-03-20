016804b8d15e52c85027bf8ff072f8f79c80c448
Shawn Sullivan <sullivansm3@gmail.com>
2020-03-19 19:22:54 -0600

    Create social media share buttons

--- src/Components/ProductOverview/DescriptionFeaturesShare/DescriptionFeaturesShare.jsx
+++ src/Components/ProductOverview/DescriptionFeaturesShare/DescriptionFeaturesShare.jsx
@@ -17,7 +17,7 @@ function renderProductFeatures(features) {
 
 const DescriptionFeatShare = ({ productData }) => (
   <div className="container-fluid">
-    <div className="row description-features-share">
+    <div className="row description-features">
       <div className="col-12 col-sm-8">
         <p className="slogan">
           {productData.slogan}
@@ -29,6 +29,17 @@ const DescriptionFeatShare = ({ productData }) => (
       <ul className="col-12 col-sm-4 list-checkmarks">
         {renderProductFeatures(productData.features)}
       </ul>
+    </div>
+    <div className="row center">
+      <div className="col-12 col-sm-12">
+        <div>Like it? Share it!</div>
+        <div>
+          <a href="#" className="fa fa-facebook" type="button" role="button"></a>
+          <a href="#" className="fa fa-twitter" type="button" role="button"></a>
+          <a href="#" className="fa fa-pinterest" type="button" role="button"></a>
+          {/* <a className="btn-floating btn-lg btn-fb" type="button" role="button"><i className="fab fa-facebook-f"></i></a> */}
+        </div>
+      </div>
     </div>
   </div>
 );
