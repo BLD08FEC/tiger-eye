2bddb2eb446e0a80747f33596c48b5b0069e1e66
Michael Morahan <morahan.mike@gmail.com>
2020-03-21 13:34:45 -0600

    added return to search

--- src/Shared/Search/search.jsx
+++ src/Shared/Search/search.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { render } from '@testing-library/react';
 
 const Search = (
+  return (  
   <div className="search">
-    <input placeholder="search for a product"> </input>
+    <input placeholder="search for a product" />
     <button type="submit" className="searchBtn">Search</button>
   </div>
+  )
 );
+
+render() {
+
+}
 
 
 export default Search;
