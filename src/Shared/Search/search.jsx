51352dc372b00f288de209ddd5317f0a05cba3fb
Michael Morahan <morahan.mike@gmail.com>
2020-03-21 15:18:39 -0600

    remove return

--- src/Shared/Search/search.jsx
+++ src/Shared/Search/search.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { render } from '@testing-library/react';
+// import { connect } from 'react-redux';
+// import { render } from '@testing-library/react';
 
-const Search = (
-  return ( 
+const Search = () => (
   <div className="search">
     <input placeholder="search for a product" />
     <button type="submit" className="searchBtn">Search</button>
   </div>
-  )
 );
-
-
 
 export default Search;
