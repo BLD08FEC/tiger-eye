dc18d97c5e54044ba7902c156f593d7eefe1504f
Shawn Sullivan <sullivansm3@gmail.com>
2020-03-20 19:54:17 -0600

    Render available stock based on style & size

--- src/Components/ProductOverview/ProductInfo/SelectQuantity.jsx
+++ src/Components/ProductOverview/ProductInfo/SelectQuantity.jsx
@@ -2,14 +2,35 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const SelectQuantity = ({ selectedStyleSKUS }) => {
+const availableStock = (availableStock) => {
+  const stock = [];
 
-  return (
-   <div>
-       Quantity selector
-   </div>
-  );
+  for (let i = 1; i <= availableStock; i++) {
+    if (i > 15) {
+      break;
+    }
+    stock.push(<option value={i} key={i}>{i}</option>);
+  }
+  return stock;
 };
+
+const SelectQuantity = ({ selectedStyleSKUS, selectedSize }) => (
+  <div>
+    <div>Qty:</div>
+    {selectedSize !== null
+      ? (
+        <select id="quantity">
+          <option value="" selected="selected" disabled hidden> 1 </option>
+          {availableStock(selectedStyleSKUS[selectedSize])}
+        </select>
+      )
+      : (
+        <select disabled value=" - ">
+          <option> - </option>
+        </select>
+      )}
+  </div>
+);
 
 const mapStateToProps = (state) => ({
   selectedStyleSKUS: state.productDataReducer.selectedStyleSKUS,
