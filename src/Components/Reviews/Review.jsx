d7f1c7a41a3a0122674d3b94f433d28266c4abdc
John Hooper <jwhooperjr@gmail.com>
2020-03-23 19:00:05 -0600

    Create Review component to render current review (dummy data)

---
+++ src/Components/Reviews/Review.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+
+class Review extends React.Component {
+    render() {
+        return (
+            <div>{this.props.review}</div>
+        )
+    }
+}
+
+export default Review;
