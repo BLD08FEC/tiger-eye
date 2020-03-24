bbe66231730adc03bc28f89340b29035d96bd3a9
Shawn Sullivan <sullivansm3@gmail.com>
2020-03-24 10:00:07 -0600

    Change styling files from css to scss

--- src/Components/ProductOverview/ProductOverview.jsx
+++ src/Components/ProductOverview/ProductOverview.jsx
@@ -1,9 +1,10 @@
+/* eslint-disable import/no-named-as-default */
 import React from 'react';
 import { connect } from 'react-redux';
 import ProductInfo from './ProductInfo/ProductInfo';
 import DescriptionFeaturesShare from './DescriptionFeaturesShare/DescriptionFeaturesShare';
 import ImageGallery from './ImageGallery/ImageGallery';
-import './ProductOverview.css';
+import './ProductOverview.scss';
 
 const ProductOverview = () => (
   <div className="product-overview">
