8492e1dceac133390c75635c8263eff6cd44e83e
thomHayner <thom.hayner@gmail.com>
2020-03-24 17:02:51 -0600

    Rename for EasyID, Add RPgetStyles, Fix initialState

--- src/data/reducers/relatedProductsReducer.jsx
+++ src/data/reducers/relatedProductsReducer.jsx
@@ -1,27 +1,36 @@
-import { GET_RELATED_PRODUCTS_SUCCESS, GET_CURRENT_CARD_SUCCESS, UPDATE_CURRENT_CARD } from '../types/types';
+import {
+  RP_GET_PRODUCT_DATA, RP_GET_RELATED_PRODUCTS, RP_GET_STYLES, RP_GET_CURRENT_CARD, RP_UPDATE_CURRENT_CARD,
+} from '../types/types';
 
 const initialState = {
-  relatedProducts: {},
-  currentCard: 0,
+  RPproductData: {},
+  RPproductName: null,
+  RPproductCategory: null,
+  RPproductPrice: null,
+  RPproductThumbnails: null,
+  RPrelatedProducts: {},
+
+  RPcurrentCard: 0,
 };
 
 const relatedProductsReducer = (state = initialState, action) => {
   switch (action.type) {
-    case GET_RELATED_PRODUCTS_SUCCESS:
+    case RP_GET_PRODUCT_DATA:
       return {
         ...state,
-        relatedProducts: action.payload,
+        RPproductData: action.payload,
+        RPproductName: action.payload.name,
+        RPproductCategory: action.payload.category,
+        RPproductPrice: action.payload.default_price,
       };
-    case GET_CURRENT_CARD_SUCCESS:
-      return {
-        ...state,
-        currentCard: action.payload, // with no API call is this still '.payload'
-      };
-    case UPDATE_CURRENT_CARD:
-      return {
-        ...state,
-        currentCard: action.payload,
-      };
+    case RP_GET_RELATED_PRODUCTS:
+      return { ...state, RPrelatedProducts: action.payload };
+    case RP_GET_STYLES:
+      return { ...state, RPproductThumbnails: action.payload.results[0].photos[0].thumbnail_url };
+    case RP_GET_CURRENT_CARD:
+      return { ...state, RPcurrentCard: action.payload };
+    case RP_UPDATE_CURRENT_CARD:
+      return { ...state, RPcurrentCard: action.payload };
     default:
       return state;
   }
