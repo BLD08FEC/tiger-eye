a2c10a5f0e0b4c813ad0a077d826d40916e712e5
John Hooper <jwhooperjr@gmail.com>
2020-03-23 19:02:10 -0600

    Refactor render method on Reviews component to iterate over list of dummy data and return a Review component while passing in each element as a prop (dummy data)

--- src/Components/Reviews/Reviews.jsx
+++ src/Components/Reviews/Reviews.jsx
@@ -1,10 +1,16 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import Review from './Review.jsx'
 
-const Reviews = () => (
-  <a id="reviews">
-    <div className="reviews">TEST</div>
-  </a>
-);
+const data = ['REVIEW NUMBER ONE', 'REVIEW NUMBER TWO', 'REVIEW NUMBER THREE'];
+
+class Reviews extends React.Component {
+  render() {
+      var list = data.map((review, index) => {
+          return (<Review review={review} key={index}/>)
+      })
+  return (<div>{list}</div>)
+  }
+};
 
 export default Reviews;
