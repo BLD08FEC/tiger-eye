b2f28929c96acc0d329cb5199ed51d176cc4bf21
Shawn Sullivan <sullivansm3@gmail.com>
2020-03-24 14:05:27 -0600

    Switch main image when thumbnails are clicked

--- src/Components/ProductOverview/ImageGallery/ImageGallery.jsx
+++ src/Components/ProductOverview/ImageGallery/ImageGallery.jsx
@@ -5,33 +5,56 @@ import './ImageGallery.scss';
 
 export class ImageGallery extends Component {
   constructor(props) {
-      super(props);
-      this.state={};
+    super(props);
+    this.state = {
+      selectedIndex: 0,
+    };
+    this.thumbnailClick = this.thumbnailClick.bind(this);
+  }
+
+  thumbnailClick(e, index) {
+    e.preventDefault();
+    this.setState({ selectedIndex: index });
   }
 
   render() {
     const { selectedStylePhotos, selectedStyleName } = this.props;
+    const { selectedIndex } = this.state;
 
     return (
       <div>
-        <div>
-            {selectedStylePhotos.length > 0
-            && <img 
-                src={selectedStylePhotos[0].url}
+          {selectedStylePhotos.length > 0
+            && (
+            <div>
+                <div className="thumbnail-list">
+                    {selectedStylePhotos.map((thumbnail, index) => (
+                        <div>
+                        <img
+                        src={thumbnail.thumbnail_url}
+                        className="thumbnail"
+                        key={index}
+                        alt={selectedStyleName}
+                        onClick={(e) => this.thumbnailClick(e, index)}
+                        />
+                        </div>
+                    ))}
+                </div>
+                <img
+                src={selectedStylePhotos[selectedIndex].url}
                 className="main-image"
                 alt={selectedStyleName}
                 />
-            }
-        </div>
+            </div>
+            )}
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
-    productStyles: state.productDataReducer.productStyles,
-    selectedStylePhotos: state.productDataReducer.selectedStylePhotos,
-    selectedStyleName: state.productDataReducer.selectedStyleName
+  productStyles: state.productDataReducer.productStyles,
+  selectedStylePhotos: state.productDataReducer.selectedStylePhotos,
+  selectedStyleName: state.productDataReducer.selectedStyleName,
 });
 
 const mapDispatchToProps = {
