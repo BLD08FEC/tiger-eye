7740f0889a3aff95caa5b6dfe8054d695586882a
John Hooper <jwhooperjr@gmail.com>
2020-03-25 09:56:35 -0600

    Import helperAPI

--- src/Components/Reviews/Reviews.jsx
+++ src/Components/Reviews/Reviews.jsx
@@ -1,10 +1,13 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import Review from './Review.jsx'
+import Review from './Review.jsx';
+import helperAPI from '../../Shared/api';
 
 const data = ['REVIEW NUMBER ONE', 'REVIEW NUMBER TWO', 'REVIEW NUMBER THREE'];
 
 class Reviews extends React.Component {
+  // helperAPI.getReviews()
+  
   render() {
       var list = data.map((review, index) => {
           return (<Review review={review} key={index}/>)
