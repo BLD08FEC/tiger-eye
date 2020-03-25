941dc2b2a5e97b009abe631150ac5180076de522
Michael Morahan <morahan.mike@gmail.com>
2020-03-25 13:55:56 -0600

    Created Answer .jsx file

---
+++ src/Components/QuestionsAndAnswers/Answers/Answer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Answer.scss";
+
+const Answer = ({ displayedAnswer, key }) => (
+  <div className="answer">
+    <div>
+      <div className="answer-body">
+        <p>A.
+            {displayedAnswer}
+        </p>
+      </div>
+    </div>
+  </div>
+);
