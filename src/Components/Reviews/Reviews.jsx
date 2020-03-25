f0ec865e9f996014ffcfa6d9631bdddea0befef4
John Hooper <jwhooperjr@gmail.com>
2020-03-25 14:42:41 -0600

    Make Reviews stateful, use componentDidMount and helperAPI to fetch data object from API and setState to update state.reviews to reviews in data object, map over this.state.reviews and pass each review as prop to returned Review component

--- src/Components/Reviews/Reviews.jsx
+++ src/Components/Reviews/Reviews.jsx
@@ -3,13 +3,29 @@ import React from 'react';
 import Review from './Review.jsx';
 import helperAPI from '../../Shared/api';
 
-const data = ['REVIEW NUMBER ONE', 'REVIEW NUMBER TWO', 'REVIEW NUMBER THREE'];
+// const data = ['REVIEW NUMBER ONE', 'REVIEW NUMBER TWO', 'REVIEW NUMBER THREE'];
 
 class Reviews extends React.Component {
   // helperAPI.getReviews()
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      reviews: []
+    }
+  }
+  componentDidMount() {
+    const reviewsAPI = [];
+    helperAPI.getReviews(1, (data) => {
+      data.results.map(review => {
+        reviewsAPI.push(review);
+        })
+      })
+      this.setState({
+        reviews: reviewsAPI
+      })
+    }
   render() {
-      var list = data.map((review, index) => {
+      var list = this.state.reviews.map((review, index) => {
           return (<Review review={review} key={index}/>)
       })
   return (<div>{list}</div>)
