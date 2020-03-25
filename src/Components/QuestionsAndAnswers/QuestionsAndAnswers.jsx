1ee8ce01ea35fe066c0c00a3dacf2a9d06191496
Michael Morahan <morahan.mike@gmail.com>
2020-03-25 13:55:36 -0600

    Imported Search Button. Reformat render to group lower bottons .

--- src/Components/QuestionsAndAnswers/QuestionsAndAnswers.jsx
+++ src/Components/QuestionsAndAnswers/QuestionsAndAnswers.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
-import QuestionList from './Questions/QuestionList';
+import React from "react";
+import QuestionList from "./Questions/QuestionList";
+import Search from "../../Shared/Search/search";
 // import helpers from '../../Shared/api';
 // require helpers from '../../shared/api.js'
 // not sure how to import info from api calls
 
 const QuestionsAndAnswers = () => (
   <div className="container questions-and-answers">
-    <div className="row questions"><em>Common Questions Realated To This Product</em></div>
+    <div className="questions">
+      <em>Common Questions Realated To This Product</em>
+    </div>
+    <hr />
+    <Search />
     <QuestionList className="row question-list" />
+    <div className="lower-btns">
+      <div className="question-btn-div">
+        <button type="button" className="question-btn">
+          Show More Answered Questions
+        </button>
+      </div>
+      <button type="button" className="question-btn">
+        Add A Question +
+      </button>
+    </div>
   </div>
 );
 
