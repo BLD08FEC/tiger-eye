45663756147b08032dce8d5e640e6b54885942ea
thomHayner <thom.hayner@gmail.com>
2020-03-26 22:59:38 -0600

    Fix Carousel Change and Length, Code Cleanup

--- src/Components/RelatedProducts/RPComponents/MyOutfitCarousel/MyOutfitCarousel.jsx
+++ src/Components/RelatedProducts/RPComponents/MyOutfitCarousel/MyOutfitCarousel.jsx
@@ -7,6 +7,7 @@ class MyOutfitCarousel extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      myOutfit: [],
       myOutfitCarouselIndex: 0,
     };
     this.next = this.next.bind(this);
@@ -15,6 +16,7 @@ class MyOutfitCarousel extends Component {
   next(direction) {
     // direction.preventDefault();
     let changeToCard = this.state.myOutfitCarouselIndex;
+    const max = this.state.myOutfit.length;
 
     if (direction === 'left') {
       changeToCard -= 1;
@@ -22,11 +24,11 @@ class MyOutfitCarousel extends Component {
     if (direction === 'right') {
       changeToCard += 1;
     }
-    if (changeToCard > 3) { // alter this to be dynamic based on number of related products
+    if (changeToCard >= max - 3) { // alter this to be dynamic based on number of related products
       changeToCard = 0;
     }
     if (changeToCard < 0) { // alter this to be dynamic based on number of related products
-      changeToCard = 2;
+      changeToCard = max - 4;
     }
 
     this.setState({ myOutfitCarouselIndex: changeToCard });
@@ -55,24 +57,6 @@ class MyOutfitCarousel extends Component {
               <div className="col-xs-3">
                 <EmptyCard />
               </div>
-              {/* <div className="col-xs-3">
-                <ProductCard
-                currentProduct={this.props.currentProduct}
-                cardIndex={this.state.carouselIndex + 1}
-                />
-                </div>
-                <div className="col-xs-3">
-                <ProductCard
-                currentProduct={this.props.currentProduct}
-                cardIndex={this.state.carouselIndex + 2}
-                />
-                </div>
-                <div className="col-xs-3">
-                <ProductCard
-                currentProduct={this.props.currentProduct}
-                cardIndex={this.state.carouselIndex + 3}
-                />
-              </div> */}
             </div>
           </div>
 
