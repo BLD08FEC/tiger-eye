1fe4167aa6334be1775f110f42b57f6761c0f15f
Michael Morahan <morahan.mike@gmail.com>
2020-03-26 21:15:34 -0600

    added ternary to account for undefined

--- src/Components/QuestionsAndAnswers/Answers/Answer.jsx
+++ src/Components/QuestionsAndAnswers/Answers/Answer.jsx
@@ -2,31 +2,36 @@ import React from 'react';
 import './Answer.scss';
 
 const Answer = ({ displayedAnswer, key }) => {
-  const formatDate = function (orig) {
-    let result = '';
-    const dateArr = orig.split('-');
-    const day = dateArr[2].slice(0, 2);
-    const month = dateArr[1];
-    const year = dateArr[0];
-    const months = ['January', 'Feburary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    const monthStr = months[month - 1];
-    result = `${monthStr} ${day}, ${year}`;
-    return result;
-  };
+  if (displayedAnswer !== undefined) {
+    const formatDate = function (orig) {
+      let result = '';
+      const dateArr = orig.split('-');
+      const day = dateArr[2].slice(0, 2);
+      const month = dateArr[1];
+      const year = dateArr[0];
+      const months = ['January', 'Feburary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+      const monthStr = months[month - 1];
+      result = `${monthStr} ${day}, ${year}`;
+      return result;
+    };
 
-  const date = formatDate(`${displayedAnswer.date}`);
+    const date = formatDate(`${displayedAnswer.date}`);
 
-  return (
-    <div className="answer">
-      <div>
-        <div className="answer-body">
-          <p>{`A. ${displayedAnswer.body} `}</p>
-        </div>
-        <div className="answer-name">
-          <p>{`by ${displayedAnswer.name}, ${date}`}</p>
+    return (
+      <div className="answer">
+        <div>
+          <div className="answer-body">
+            <p>{`A. ${displayedAnswer.body} `}</p>
+          </div>
+          <div className="answer-name">
+            <p>{`by ${displayedAnswer.name}, ${date}`}</p>
+          </div>
         </div>
       </div>
-    </div>
+    );
+  }
+  return (
+    <div />
   );
 };
 
