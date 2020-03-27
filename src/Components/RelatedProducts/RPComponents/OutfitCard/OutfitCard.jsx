139ee8853b3b319a41d919a874145d3d2ef0f042
thomHayner <thom.hayner@gmail.com>
2020-03-26 21:51:24 -0600

    Add Descriptive Text

--- src/Components/RelatedProducts/RPComponents/OutfitCard/OutfitCard.jsx
+++ src/Components/RelatedProducts/RPComponents/OutfitCard/OutfitCard.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import './OutfitCard.scss';
 
-const OutfitCard = () => (<div ></div>);
+const OutfitCard = () => (
+  <div className="add-item">
+    + <br /> Add current item to your outfit
+  </div>
+);
 
 
 export default OutfitCard;
