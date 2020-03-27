54e0c07f75bd610b69108b23298a236a1d626b32
Shawn Sullivan <sullivansm3@gmail.com>
2020-03-27 08:24:43 -0600

    Deconstruct props

--- src/Components/ProductOverview/ProductInfo/Styles.jsx
+++ src/Components/ProductOverview/ProductInfo/Styles.jsx
@@ -1,21 +1,17 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
-/* eslint-disable react/jsx-one-expression-per-line */
-/* eslint-disable react/destructuring-assignment */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/*eslint-disable*/
 import React from 'react';
 import { connect } from 'react-redux';
 import '../ProductOverview.css';
 import { updateSelectedStyle } from '../../../data/actions/productDataAction';
 
-const Styles = (props) => (
-  props.productStyles.product_id ? (
+const Styles = ({ productStyles, selectedStyleName, selectedStyle_id, updateSelectedStyle, selectedStyleSKUS }) => (
+  productStyles.product_id ? (
     <div className="select-checkmark">
-      <div className="style-name">Style: {props.selectedStyleName}</div>
-      <div>
-        {props.productStyles.results.map((style, index) => (
+      <div className="style-name">Style: {selectedStyleName}</div>
+      <div>    
+        {productStyles.results.map((style, index) => (
           <label className="style-container" key={style.style_id}>
-            {props.selectedStyle_id === style.style_id
+            {selectedStyle_id === style.style_id
               ? <input type="radio" name="style-selector" checked />
               : <input type="radio" name="style-selector" />}
             <span className="checkmark">✔</span>
@@ -25,7 +21,7 @@ const Styles = (props) => (
               id={index}
               className="rounded-circle style-pics"
               name="style-selector"
-              onClick={() => props.updateSelectedStyle(props.productStyles.results[index])}
+              onClick={() => updateSelectedStyle(productStyles.results[index])}
             />
           </label>
         ))}
@@ -39,6 +35,7 @@ const mapStateToProps = (state) => ({
   productStyles: state.productDataReducer.productStyles,
   selectedStyleName: state.productDataReducer.selectedStyleName,
   selectedStyle_id: state.productDataReducer.selectedStyle_id,
+  selectedStyleSKUS: state.productDataReducer.selectedStyleSKUS,
 });
 
 const mapDispatchToProps = (dispatch) => ({
