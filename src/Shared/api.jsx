6f2033e52c0604d8f00507972860b6e9298dcd4c
Michael Morahan <morahan.mike@gmail.com>
2020-03-28 12:32:27 -0600

    deleted unnecessary comments and code

--- src/Shared/api.jsx
+++ src/Shared/api.jsx
@@ -22,7 +22,7 @@ import axios from 'axios';
 
 const helperAPI = {
   getCart(userToken, cb) {
-    axios.get(`http://3.134.102.30/cart/${userToken}`)
+    axios.get(`http://52.26.193.201:3000/cart/${userToken}`)
       // .then((res) => console.log(res.data));
       .then((res) => {
         cb(res.data);
@@ -34,7 +34,7 @@ const helperAPI = {
 
   // post: /cart/
   addToCart(userToken, cb) {
-    axios.post('http://3.134.102.30/cart/')
+    axios.post('http://52.26.193.201:3000/cart/')
       // .then((res) => console.log(res.data));
       .then((res) => cb(res.data))
       .catch((err) => cb(err));
@@ -43,7 +43,7 @@ const helperAPI = {
   // ======= Interactions API ==============
 
   logInteraction(userToken, cb) {
-    axios.post('http://3.134.102.30/interactions')
+    axios.post('http://52.26.193.201:3000/interactions')
       // .then((res) => console.log(res.data));
       .then((res) => cb(res.data))
       .catch((err) => cb(err));
@@ -52,21 +52,21 @@ const helperAPI = {
   // ======= Products =====================
   // GET / products / list
   getProducts(productId, cb) {
-    axios.get('http://3.134.102.30/products/list')
+    axios.get('http://52.26.193.201:3000/products/list')
       // .then((res) => console.log(res.data));
       .then((res) => cb(res.data))
       .catch((err) => cb(err));
   },
 
   getProduct(productId, cb) {
-    axios.get(`http://3.134.102.30/products/${productId}`)
+    axios.get(`http://52.26.193.201:3000/products/${productId}`)
       // .then((res) => console.log(res.data));
       .then((res) => cb(res.data))
       .catch((err) => cb(err));
   },
 
   getProductStyle(productId, cb) {
-    axios.get(`http://3.134.102.30/products/${productId}/styles`)
+    axios.get(`http://52.26.193.201:3000/products/${productId}/styles`)
       // .then((res) => console.log(res.data));
       .then((res) => cb(res.data))
       .catch((err) => cb(err));
@@ -76,7 +76,7 @@ const helperAPI = {
   // ======= Related Products ==============
 
   getRelatedProduct(productId, cb) {
-    axios.get(`http://3.134.102.30/products/${productId}/related`)
+    axios.get(`http://52.26.193.201:3000/products/${productId}/related`)
       // .then((res) => console.log(res.data));
       .then((res) => cb(res.data))
       .catch((err) => cb(err));
@@ -87,7 +87,7 @@ const helperAPI = {
   // Get
 
   getQuestions(productId, cb) {
-    axios.get(`http://3.134.102.30/qa/${productId}`)
+    axios.get(`http://52.26.193.201:3000/qa/${productId}`)
       .then((res) => cb(res.data));
     // .then((res) => console.log('res ======= ', res.data));
     // .catch((err) => cb(err));
@@ -95,14 +95,14 @@ const helperAPI = {
   },
 
   // getQuestions(productId, cb) {
-  //   axios.get(`http://3.134.102.30/qa/${productId}`)
+  //   axios.get(`http://52.26.193.201:3000/qa/${productId}`)
   //     // .then((res) => console.log(res.data));
   //     .then((res) => cb(res.data))
   //     .catch((err) => cb(err));
   // },
 
   getAnswers(questionId, cb) {
-    axios.get(`http://3.134.102.30/qa/${questionId}/answers`)
+    axios.get(`http://52.26.193.201:3000/qa/${questionId}/answers`)
       // .then((res) => console.log(res.data));
       .then((res) => cb(res.data))
       .catch((err) => cb(err));
@@ -110,14 +110,14 @@ const helperAPI = {
 
   // Post
   postQuestion(productId, cb) {
-    axios.post(`http://3.134.102.30/qa/${productId}`)
+    axios.post(`http://52.26.193.201:3000/qa/${productId}`)
       // .then((res) => console.log(res.data));
       .then((res) => cb(res.data))
       .catch((err) => cb(err));
   },
 
   postAnswer(questionId, cb) {
-    axios.post(`http://3.134.102.30/qa/${questionId}/answers`)
+    axios.post(`http://52.26.193.201:3000/qa/${questionId}/answers`)
       // .then((res) => console.log(res.data));
       .then((res) => cb(res.data))
       .catch((err) => cb(err));
@@ -127,14 +127,14 @@ const helperAPI = {
   // PUT / qa / question /: question_id / helpful
 
   markQuestionHelpful(questionId, cb) {
-    axios.put(`http://3.134.102.30/qa/question/${questionId}/helpful`)
+    axios.put(`http://52.26.193.201:3000/qa/question/${questionId}/helpful`)
       // .then((res) => console.log(res.data));
       .then((res) => cb(res.data))
       .catch((err) => cb(err));
   },
 
   reportQuestion(questionId, cb) {
-    axios.put(`http://3.134.102.30/qa/question/${questionId}/report`)
+    axios.put(`http://52.26.193.201:3000/qa/question/${questionId}/report`)
       // .then((res) => console.log(res.data));
       .then((res) => cb(res.data))
       .catch((err) => cb(err));
@@ -144,14 +144,14 @@ const helperAPI = {
   // PUT / qa / answer /: question_id / helpful
 
   markAnswerHelpful(answerId, cb) {
-    axios.put(`http://3.134.102.30/qa/answer/${answerId}/helpful`)
+    axios.put(`http://52.26.193.201:3000/qa/answer/${answerId}/helpful`)
       // .then((res) => console.log(res.data));
       .then((res) => cb(res.data))
       .catch((err) => cb(err));
   },
 
   reportAnswer(answerId, cb) {
-    axios.put(`http://3.134.102.30/qa/answer/${answerId}/report`)
+    axios.put(`http://52.26.193.201:3000/qa/answer/${answerId}/report`)
       // .then((res) => console.log(res.data));
       .then((res) => cb(res.data))
       .catch((err) => cb(err));
@@ -162,237 +162,39 @@ const helperAPI = {
 
   // GET /reviews/:product_id/list
   getReviews(productId, cb) {
-    axios.get(`http://3.134.102.30/reviews/${productId}/list`)
+    axios.get(`http://52.26.193.201:3000/reviews/${productId}/list`)
       // .then((res) => console.log(res.data));
       .then((res) => cb(res.data))
       .catch((err) => cb(err));
   },
 
   getReviewMeta(productId, cb) {
-    axios.get(`http://3.134.102.30/reviews/${productId}/meta`)
+    axios.get(`http://52.26.193.201:3000/reviews/${productId}/meta`)
       // .then((res) => console.log(res.data));
       .then((res) => cb(res.data))
       .catch((err) => cb(err));
   },
 
   postReview(productId, cb) {
-    axios.post(`http://3.134.102.30/reviews/${productId}`)
+    axios.post(`http://52.26.193.201:3000/reviews/${productId}`)
       // .then((res) => console.log(res.data));
       .then((res) => cb(res.data))
       .catch((err) => cb(err));
   },
 
   markReviewHelpful(reviewId, cb) {
-    axios.put(`http://3.134.102.30/reviews/helpful/${reviewId}`)
+    axios.put(`http://52.26.193.201:3000/reviews/helpful/${reviewId}`)
       // .then((res) => console.log(res.data));
       .then((res) => cb(res.data))
       .catch((err) => cb(err));
   },
 
   reportReview(reviewId, cb) {
-    axios.put(`http://3.134.102.30/reviews/report/${reviewId}`)
+    axios.put(`http://52.26.193.201:3000/reviews/report/${reviewId}`)
       // .then((res) => console.log(res.data));
       .then((res) => cb(res.data))
       .catch((err) => cb(err));
   },
 };
-
-
-// ======= ORIGINAL CODE BELOW ===========
-// \\//\\//\\//\\//\\//\\//\\//\\//\\//\\//
-// ======== Cart API =====================
-
-// // get   = /cart/:user_token
-// const getCart = (userToken, cb) => {
-//   axios.get(`http://3.134.102.30/cart/${userToken}`)
-//     .then((res) => console.log(res.data));
-//   // .then(res.data => cb(res.data))
-//   // .catch(err => cb(error))
-// };
-
-// // post = /cart/
-// const addToCart = (userToken, cb) => {
-//   axios.post('http://3.134.102.30/cart/')
-//     .then((res) => console.log(res.data));
-//   // .then(res.data => cb(res.data))
-//   // .catch(err => cb(error))
-// };
-
-// // ======== Interactions API ==============
-
-// const logInteraction = (userToken, cb) => {
-//   axios.post('http://3.134.102.30/interactions')
-//     .then((res) => console.log(res.data));
-//   // .then(res.data => cb(res.data))
-//   // .catch(err => cb(error))
-// };
-
-// // ======== Products =====================
-// // GET / products / list
-// const getProducts = (productId, cb) => {
-//   axios.get('http://3.134.102.30/products/list')
-//     .then((res) => console.log(res.data));
-//   // .then(res.data => cb(res.data))
-//   // .catch(err => cb(error))
-// };
-
-// const getProduct = (productId, cb) => {
-//   axios.get(`http://3.134.102.30/products/${productId}`)
-//     .then((res) => console.log(res.data));
-//   // .then(res.data => cb(res.data))
-//   // .catch(err => cb(error))
-// };
-
-// const getProductStyle = (productId, cb) => {
-//   axios.get(`http://3.134.102.30/products/${productId}/styles`)
-//     .then((res) => console.log(res.data));
-//   // .then(res.data => cb(res.data))
-//   // .catch(err => cb(error))
-// };
-
-
-// // ======== Related Products ==============
-
-// const getRelatedProduct = (productId, cb) => {
-//   axios.get(`http://3.134.102.30/products/${productId}/related`)
-//     .then((res) => console.log(res.data));
-//   // .then(res.data => cb(res.data))
-//   // .catch(err => cb(error))
-// };
-
-
-// // ======== Question and Answers ==========
-
-// // Get
-// const getQuestions = (productId, cb) => {
-//   axios.get(`http://3.134.102.30/qa/${productId}`)
-//     .then((res) => console.log(res.data));
-//   // .then(res.data => cb(res.data))
-//   // .catch(err => cb(error))
-// };
-
-// const getAnswers = (questionId, cb) => {
-//   axios.get(`http://3.134.102.30/qa/${questionId}/answers`)
-//     .then((res) => console.log(res.data));
-//   // .then(res.data => cb(res.data))
-//   // .catch(err => cb(error))
-// };
-
-// // Post
-// const postQuestion = (productId, cb) => {
-//   axios.post(`http://3.134.102.30/qa/${productId}`)
-//     .then((res) => console.log(res.data));
-//   // .then(res.data => cb(res.data))
-//   // .catch(err => cb(error))
-// };
-
-// const postAnswer = (questionId, cb) => {
-//   axios.post(`http://3.134.102.30/qa/${questionId}/answers`)
-//     .then((res) => console.log(res.data));
-//   // .then(res.data => cb(res.data))
-//   // .catch(err => cb(error))
-// };
-
-// // review Question
-// // PUT / qa / question /: question_id / helpful
-
-// const markQuestionHelpful = (questionId, cb) => {
-//   axios.put(`http://3.134.102.30/qa/question/${questionId}/helpful`)
-//     .then((res) => console.log(res.data));
-//   // .then(res.data => cb(res.data))
-//   // .catch(err => cb(error))
-// };
-
-// const reportQuestion = (questionId, cb) => {
-//   axios.put(`http://3.134.102.30/qa/question/${questionId}/report`)
-//     .then((res) => console.log(res.data));
-//   // .then(res.data => cb(res.data))
-//   // .catch(err => cb(error))
-// };
-
-// // review Answer
-// // PUT / qa / answer /: question_id / helpful
-
-// const markAnswerHelpful = (answerId, cb) => {
-//   axios.put(`http://3.134.102.30/qa/answer/${answerId}/helpful`)
-//     .then((res) => console.log(res.data));
-//   // .then(res.data => cb(res.data))
-//   // .catch(err => cb(error))
-// };
-
-// const reportAnswer = (answerId, cb) => {
-//   axios.put(`http://3.134.102.30/qa/answer/${answerId}/report`)
-//     .then((res) => console.log(res.data));
-//   // .then(res.data => cb(res.data))
-//   // .catch(err => cb(error))
-// };
-
-
-// // =========== Reviews ============
-
-// // GET /reviews/:product_id/list
-// const getReviews = (productId, cb) => {
-//   axios.get(`http://3.134.102.30/reviews/${productId}/list`)
-//     .then((res) => console.log(res.data));
-//   // .then(res.data => cb(res.data))
-//   // .catch(err => cb(error))
-// };
-
-// const getReviewMeta = (productId, cb) => {
-//   axios.get(`http://3.134.102.30/reviews/${productId}/meta`)
-//     .then((res) => console.log(res.data));
-//   // .then(res.data => cb(res.data))
-//   // .catch(err => cb(error))
-// };
-
-// const postReview = (productId, cb) => {
-//   axios.post(`http://3.134.102.30/reviews/${productId}`)
-//     .then((res) => console.log(res.data));
-//   // .then(res.data => cb(res.data))
-//   // .catch(err => cb(error))
-// };
-
-// const markReviewHelpful = (reviewId, cb) => {
-//   axios.put(`http://3.134.102.30/reviews/helpful/${reviewId}`)
-//     .then((res) => console.log(res.data));
-//   // .then(res.data => cb(res.data))
-//   // .catch(err => cb(error))
-// };
-
-// const reportReview = (reviewId, cb) => {
-//   axios.put(`http://3.134.102.30/reviews/report/${reviewId}`)
-//     .then((res) => console.log(res.data));
-//   // .then(res.data => cb(res.data))
-//   // .catch(err => cb(error))
-// };
-
-
-// debating between Add vs Post for naming methods.
-// ex: addQuestion / addAnswer vs postQuestion / postAnswer
-// applies to reviews & questions post methods
-// generally add is more intuitive but post is the type of request...
-
-// module.exports(
-// logInteraction,
-// getCart,
-// addToCart,
-// getProducts,
-// getProduct,
-// getProductStyle,
-// getRelatedProduct,
-// getQuestions,
-// getAnswers,
-// postQuestion,
-// postAnswer,
-// markQuestionHelpful,
-// reportQuestion,
-// markAnswerHelpful,
-// reportAnswer,
-// getReviews,
-// getReviewMeta,
-// postReview,
-// markReviewHelpful,
-// reportReview,
-// );
 
 export default helperAPI;
