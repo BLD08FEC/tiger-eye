a3a1c3bb2e8fe356c6e3eda27340e0ed1daba496
thomHayner <thom.hayner@gmail.com>
2020-03-28 15:20:58 -0600

    Change From Button to Div, Fix onClick

--- src/Components/RelatedProducts/RPComponents/OutfitCard/OutfitCard.jsx
+++ src/Components/RelatedProducts/RPComponents/OutfitCard/OutfitCard.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './OutfitCard.scss';
 
 const OutfitCard = (props) => (
-  <button type="button" className="add-item" onClick={() => props.handleClick}>
+  <div className="add-item" onClick={props.handleClick}>
     +
     <br />
     Add current item to your outfit
-  </button>
+  </div>
 );
 
 
