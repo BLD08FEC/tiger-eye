4a6e09cce683201b0bbb65e858eead5f6610f581
thomHayner <thom.hayner@gmail.com>
2020-03-28 23:34:30 -0600

    Link Card Buttons To Modal Dashboard

--- src/Components/RelatedProducts/RPComponents/ProductCardImage/ProductCardImage.jsx
+++ src/Components/RelatedProducts/RPComponents/ProductCardImage/ProductCardImage.jsx
@@ -2,6 +2,7 @@
 /* eslint-disable no-undef */
 import React, { useState, useEffect } from 'react';
 import './ProductCardImage.scss';
+import ModalDashboard from '../modal/ModalDashboard/ModalDashboard';
 // import helperApi from '../../../../Shared/api';
 
 const ProductCardImage = (props) => {
@@ -17,14 +18,14 @@ const ProductCardImage = (props) => {
   if (imageUrl == null) {
     return (
       <div className="card-image" onClick={() => {}} onKeyPress={() => { /* if {props.carouselType} === suggested, then redirect entire page to this card's product id */ }} role="button" tabIndex={0}>
-        <div className="button-overlay" onClick={() => { /* if {props.carouselType} === suggested, then open modal; if {props.carouselType} === myOutfit, then remove from myOutfit */ }} onKeyPress={() => {}} role="button" tabIndex={0}>{props.buttonType}</div>
+        <div className="button-overlay" onClick={() => { /* if {props.carouselType} === suggested, then open modal; if {props.carouselType} === myOutfit, then remove from myOutfit */ }} onKeyPress={() => {}} role="button" tabIndex={0}><ModalDashboard buttonType={props.buttonType} carouselType={props.carouselType} /></div>
         <img src="https://www.warnersstellian.com/Content/images/product_image_not_available.png" alt="not available" />
       </div>
     );
   }
   return (
     <div className="card-image" onClick={() => { /* if {props.carouselType} === suggested, then redirect entire page to this card's product id */ }} onKeyPress={() => {}} role="button" tabIndex={0}>
-      <div className="button-overlay" onClick={() => { /* if {props.carouselType} === suggested, then redirect entire page to this card's product id */ }} onKeyPress={() => {}} role="button" tabIndex={0}>{props.buttonType}</div>
+      <div className="button-overlay" onClick={() => { /* if {props.carouselType} === suggested, then redirect entire page to this card's product id */ }} onKeyPress={() => {}} role="button" tabIndex={0}><ModalDashboard buttonType={props.buttonType} carouselType={props.carouselType} /></div>
       <img src={imageUrl} alt="product" />
     </div>
   );
