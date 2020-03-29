b46bc93c8514034010411f4b2e9c0e081a0952c6
thomHayner <thom.hayner@gmail.com>
2020-03-28 22:39:02 -0600

    Fix / Ignore Problem

--- src/Components/RelatedProducts/RPComponents/OutfitCard/OutfitCard.jsx
+++ src/Components/RelatedProducts/RPComponents/OutfitCard/OutfitCard.jsx
@@ -1,8 +1,9 @@
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
 import './OutfitCard.scss';
 
 const OutfitCard = (props) => (
-  <div className="add-item" onClick={props.handleClick}>
+  <div className="add-item" onClick={props.handleClick} onKeyPress={() => {}} role="button" tabIndex={0}>
     +
     <br />
     Add current item to your outfit
