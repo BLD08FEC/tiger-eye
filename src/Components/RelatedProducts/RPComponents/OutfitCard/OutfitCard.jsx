f86f6749bb218b93c9bc6cbf03ea48da36cf9bd1
thomHayner <thom.hayner@gmail.com>
2020-03-29 18:34:23 -0600

    Expanded div Props for Readability

--- src/Components/RelatedProducts/RPComponents/OutfitCard/OutfitCard.jsx
+++ src/Components/RelatedProducts/RPComponents/OutfitCard/OutfitCard.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 import './OutfitCard.scss';
 
 const OutfitCard = (props) => (
-  <div className="rp-add-item-button" onClick={props.handleClick} onKeyPress={() => {}} role="button" tabIndex={0}>
+  <div
+    className="rp-add-item-button"
+    onClick={props.handleClick}
+    onKeyPress={() => {}}
+    role="button"
+    tabIndex={0}
+  >
     +
     <br />
     Add current item to your outfit
